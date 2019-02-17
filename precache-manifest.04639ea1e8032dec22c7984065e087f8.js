5b17c446d111f84bf929280da4829bce799253c8
stereobooster <stereobooster@gmail.com>
2019-02-17 13:41:17 +0100

    Updates

---
+++ precache-manifest.04639ea1e8032dec22c7984065e087f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e2e49d08311fb525d5df",
+    "url": "/static/js/main.e2e49d08.chunk.js"
+  },
+  {
+    "revision": "c1e9718a3224ddce6feb",
+    "url": "/static/js/2.c1e9718a.chunk.js"
+  },
+  {
+    "revision": "e2e49d08311fb525d5df",
+    "url": "/static/css/main.877df88f.chunk.css"
+  },
+  {
+    "revision": "bcf4918f61a79ad38d21a6a6e5e1d8e5",
+    "url": "/index.html"
+  }
+];
